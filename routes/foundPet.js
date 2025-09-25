a7cca139f2d0e500f62205c5d8e7a9d65ad0d1de
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-09-25 21:25:17 +0530

    updated route

--- routes/foundPet.js
+++ routes/foundPet.js
@@ -1,9 +1,27 @@
 // routes/foundPet.js
 const express = require("express");
 const router = express.Router();
-const { createFoundPet } = require("../controllers/foundPetController");
+const {
+  createFoundPet,
+  getAllFoundPets,
+  getFoundPetById,
+  updateFoundPet,
+  deleteFoundPet
+} = require("../controllers/foundPetController");
 
-// POST /found-pets
+// CREATE
 router.post("/", createFoundPet);
+
+// READ all
+router.get("/", getAllFoundPets);
+
+// READ one
+router.get("/:id", getFoundPetById);
+
+// UPDATE
+router.put("/:id", updateFoundPet);
+
+// DELETE
+router.delete("/:id", deleteFoundPet);
 
 module.exports = router;
