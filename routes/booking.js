cf35aa43b1a71c7435a20297d9bf22848c2437b7
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-09-26 17:32:02 +0530

    made routes

---
+++ routes/booking.js
@@ -0,0 +1,13 @@
+// routes/bookings.js
+const express = require('express');
+const router = express.Router();
+const asyncHandler = require('../middlewares/asyncHandler');
+const bookingsController = require('../controllers/bookingsController');
+
+router.post('/', asyncHandler(bookingsController.createBooking));
+router.get('/', asyncHandler(bookingsController.getAllBookings));
+router.get('/:id', asyncHandler(bookingsController.getBookingById));
+router.put('/:id', asyncHandler(bookingsController.updateBooking));
+router.delete('/:id', asyncHandler(bookingsController.deleteBooking));
+
+module.exports = router;
