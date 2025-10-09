7ea760b2905571fb0b682c6c8f4319bab7dd8d3b
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-10-09 21:42:00 +0530

    updated lost pet model

--- models/lostPetModel.js
+++ models/lostPetModel.js
@@ -1,3 +1,4 @@
+// models/lostPetModel.js
 const Joi = require("joi");
 
 const locationSchema = Joi.object({
@@ -6,7 +7,7 @@ const locationSchema = Joi.object({
   address: Joi.string().required(),
 });
 
-const createLostPetSchema = Joi.object({
+const lostPetSchema = Joi.object({
   images: Joi.array()
     .items(Joi.string().uri())
     .min(1)
@@ -16,42 +17,21 @@ const createLostPetSchema = Joi.object({
       "array.min": "At least 1 image is required.",
       "array.max": "You can upload a maximum of 3 images.",
     }),
+
   name: Joi.string().trim().required(),
   age: Joi.number().min(0).required(),
   color: Joi.string().trim().required(),
-  breed: Joi.string().trim().required(),
   height: Joi.string().trim().required(),
   weight: Joi.string().trim().required(),
-  location: locationSchema.required(),
-  description: Joi.string().trim().required(),
+  breed: Joi.string().trim().required(),
   gender: Joi.string().valid("Male", "Female", "Prefer Not to Say").required(),
+  description: Joi.string().trim().required(),
+
+  location: locationSchema.required(),
+
   userId: Joi.string().required(),
-  postType: Joi.string().valid("Lost").required(),
+  postType: Joi.string().valid("Lost").required(), // must be "Lost"
+  createdAt: Joi.date().iso().default(() => new Date().toISOString()),
 });
 
-const updateLostPetSchema = Joi.object({
-  images: Joi.array()
-    .items(Joi.string().uri())
-    .min(1)
-    .max(3)
-    .messages({
-      "array.min": "At least 1 image is required.",
-      "array.max": "You can upload a maximum of 3 images.",
-    }),
-  name: Joi.string().trim(),
-  age: Joi.number().min(0),
-  color: Joi.string().trim(),
-  breed: Joi.string().trim(),
-  height: Joi.string().trim(),
-  weight: Joi.string().trim(),
-  location: locationSchema,
-  description: Joi.string().trim(),
-  gender: Joi.string().valid("Male", "Female","Prefer Not to Say"),
-  userId: Joi.string(),
-  postType: Joi.string().valid("Lost"),
-});
-
-module.exports = {
-  createLostPetSchema,
-  updateLostPetSchema,
-};
+module.exports = { lostPetSchema };
