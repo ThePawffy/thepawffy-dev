39234cd7006262fb5fd1abdc4aa212631dfa75fa
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-10-13 19:30:26 +0530

    changes

--- routes/pet.js
+++ routes/pet.js
@@ -2,25 +2,22 @@ const express = require("express");
 const router = express.Router();
 const petController = require("../controllers/petController");
 
-// CREATE PET(S) for a user
+// CREATE or UPDATE PET(S)
 router.post("/", petController.createPet);
 
 // READ ALL PETS
 router.get("/", petController.getAllPets);
 
-// READ PET(S)
-// By parent document ID: /api/pets/doc/:id
+// READ PET BY DOCUMENT ID
 router.get("/doc/:id", petController.getPet);
 
-// By single pet ID: /api/pets/pet/:petId
+// READ SINGLE PET BY PET ID
 router.get("/pet/:petId", petController.getPet);
 
-// UPDATE PET
-// Update single pet by petId
-router.put("/pet/:petId", petController.updatePet);
+// UPDATE PET BY PET ID
+router.put("/:petId", petController.updatePet);
 
-// DELETE PET
-// Delete single pet by petId
-router.delete("/pet/:petId", petController.deletePet);
+// DELETE PET BY PET ID
+router.delete("/:petId", petController.deletePet);
 
 module.exports = router;
