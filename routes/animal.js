5bc43178300be74ec8859cd9efd034ed874a41bc
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-10-16 16:19:14 +0530

    changes

--- routes/animal.js
+++ routes/animal.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAnimalData } = require("../controllers/animalController");
+const {
+  getAnimalData,
+  getAllAnimalTypes
+} = require("../controllers/animalController");
 
+// ✅ POST: Get species by animalTypeId
 router.post("/get-animal-data", getAnimalData);
+
+// ✅ GET: Get all animal types
 router.get("/get-animal-types", getAllAnimalTypes);
 
 module.exports = router;
