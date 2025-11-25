dbdc737bde0ac344e7dd49fe00e802945433e1db
Yash Verma <yv48183@gmail.com>
2025-11-25 13:44:21 +0530

    fix: port

--- server.js
+++ server.js
@@ -91,5 +91,9 @@ app.use(errorHandler);
 // ------------------------------------------------------------
 // Start Server
 // ------------------------------------------------------------
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+});
+
