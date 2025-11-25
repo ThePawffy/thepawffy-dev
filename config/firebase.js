0658fa0d894721bc5eaddfa99f616cb0d985a9d5
Yash Verma <yv48183@gmail.com>
2025-11-25 16:46:13 +0530

    fix: the firebase private key

--- config/firebase.js
+++ config/firebase.js
@@ -1,21 +1,12 @@
 const admin = require("firebase-admin");
 
-let serviceAccount = null;
-
 if (!admin.apps.length) {
-  // Decode Base64 secret stored in Fly.io
-  const base64Key = process.env.FIREBASE_KEY_BASE64;
-
-  if (!base64Key) {
-    throw new Error("❌ FIREBASE_KEY_BASE64 environment secret is missing!");
-  }
-
-  // Convert Base64 → JSON string → Object
-  const decodedKey = Buffer.from(base64Key, "base64").toString("utf8");
-  serviceAccount = JSON.parse(decodedKey);
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
